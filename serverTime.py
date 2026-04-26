ba9bb86cced8c70245333624fa71836caa7cf6a9
Tarik366 <tariky113@gmail.com>
2026-04-26 22:33:28 +0300

    Fixed server times

--- serverTime.py
+++ serverTime.py
@@ -3,14 +3,12 @@ from pytz import timezone
 import tkinter as tk
 
 ServerTimezones = {
-    "America": "Etc/GMT+5",
-    "Europe": "Etc/GMT+1",
-    "Asia": "Etc/GMT-8"
+    "America": "Etc/GMT+9",
+    "Europe": "Etc/GMT+5",
+    "Asia": "Etc/GMT-4"
 }
 
 def getServerTime(serv, loc):
-    print(serv)
-    print(loc)
     locale.setlocale(locale.LC_TIME, f"{Config.get_locale_code(loc)}.UTF-8")
     now = datetime.datetime.now(timezone(ServerTimezones[serv]))
     return now.strftime("%x %a").encode(locale.getlocale()[1], "backslashreplace").decode()
