9f131cf925e0ca306cf8921c738812dbd9bbba33
Tarik366 <tariky113@gmail.com>
2026-04-26 03:02:06 +0300

    Added ComboBox Form

--- overlay.py
+++ overlay.py
@@ -1,14 +1,29 @@
 import tkinter as tk
+from tkinter import ttk
 import serverTime
 import floatingWindow
 
 root = floatingWindow.App()
 
 root.title("Genshin Todo Calendar")
+root.geometry("500x500+200+400")
+# Focus to config window when app started
+root.lift()
+
+ComboBoxForm: dict[str, list] = {
+    "Server": list(serverTime.ServerTimezones.keys()),
+    "Language": ["English", "Turkish", "Japanese"]
+}
+
+i=0
+for lab, boxValues in ComboBoxForm.items():
+    i+=1
+    tk.Label(root, text=lab).grid(row=i, column=0)
+    ttk.Combobox(root, width=27, values=boxValues).grid(row=i, column=1)
 
 @root.floater.add_widget
 def date(parent):
-    return tk.Label(parent, text=serverTime.date, font=("Noto Sans", 16, "bold"), fg="white", bg="black")
+    return tk.Label(parent, text=serverTime.date, fg="white", bg="black")
 
 @root.floater.add_widget
 def overlay(parent):
@@ -16,7 +31,6 @@ def overlay(parent):
                   width = 15, 
                   bg = "black",
                   activestyle = 'dotbox', 
-                  font = "Default_SC-85W",
                   fg = "white")
 
     Items = [
